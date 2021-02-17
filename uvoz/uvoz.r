4963117b07f100dc32d1b0ba33a969e2dc39966e
NikiFMF10 <49947147+NikiFMF10@users.noreply.github.com>
2021-02-17 21:54:01 +0100

    Preurejen uvoz

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -3,62 +3,102 @@
 library(readr)
 library(tidyr)
 library(dplyr)
+library(reshape2)
 
 sl <- locale("sl", decimal_mark=",", grouping_mark=".")
 
-# Funkcija, ki uvozi občine iz Wikipedije
-uvozi.obcine <- function() {
-  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-  stran <- html_session(link) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec=",")
-  for (i in 1:ncol(tabela)) {
-    if (is.character(tabela[[i]])) {
-      Encoding(tabela[[i]]) <- "UTF-8"
-    }
-  }
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    if (is.character(tabela[[col]])) {
-      tabela[[col]] <- parse_number(tabela[[col]], na="-", locale=sl)
-    }
-  }
-  for (col in c("obcina", "pokrajina", "regija")) {
-    tabela[[col]] <- factor(tabela[[col]])
-  }
-  return(tabela)
-}
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function(obcine) {
-  data <- read_csv2("podatki/druzine.csv", col_names=c("obcina", 1:4),
+# POTNIŠKI PREVOZ IN PROMET
+promet <- read_csv2("podatki/Potniski_prevoz_in_promet.csv", col_names = c("Potniki(1000)", "Vrsta_prevoza", 2002:2018),
+                    skip = 4, n_max = 8, na = "...",
                     locale=locale(encoding="Windows-1250"))
-  data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-    strapplyc("([^ ]+)") %>% sapply(paste, collapse=" ") %>% unlist()
-  data$obcina[data$obcina == "Sveti Jurij"] <- iconv("Sveti Jurij ob Ščavnici", to="UTF-8")
-  data <- data %>% pivot_longer(`1`:`4`, names_to="velikost.druzine", values_to="stevilo.druzin")
-  data$velikost.druzine <- parse_number(data$velikost.druzine)
-  data$obcina <- parse_factor(data$obcina, levels=obcine)
-  return(data)
-}
+promet <- promet[,-1]
 
-# Zapišimo podatke v razpredelnico obcine
-obcine <- uvozi.obcine()
-
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine(levels(obcine$obcina))
-
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+promet <- melt(promet, id.vars = "Vrsta_prevoza", measure.vars = names(promet)[-1],
+               variable.name = "Leto", value.name = "Stevilo",
+               na.rm = TRUE)
 
 
-tabela <- read_csv2('podatki/Cestni promet v Sloveniji.csv',skip=1,
-                    col_names=c('Vrsta vozila','Občine', 'Leto', 'Število vseh vozil'),
-                    locale=locale(encoding='Windows-1250'))
+# ŠTEVILO AVTOMOBILOV GLEDE NA VRSTO POGONA 
+
+pogon <- read_csv2("podatki/Prevozna_sredstva_glede_na_pogon_in_gorivo.csv", col_names = c("Osebni avtomobili","Leto", "Vrsta_pogona", "Stevilo_avtomobilov"),
+                   skip = 6, n_max=36,
+                   locale = locale(encoding = "Windows-1250"))
+pogon <- pogon[c(3,2,4)] %>% fill(2)
+
+pogon <- filter(pogon, Vrsta_pogona !='NA')
+pogon <- filter(pogon, Vrsta_pogona !='Vrsta pogona in goriva - SKUPAJ')
+
+
+
+indeksi <- dcast(pogon, Vrsta_pogona~Leto, value.var = 'Stevilo_avtomobilov' )
+colnames(indeksi) <- c("Vrsta_pogona", "a", "b", "c", "d")
+indeksi <- transform(indeksi, Indeks1 = (b-a)*100/a)
+indeksi <- transform(indeksi, Indeks2 = (c-b)*100/b)
+indeksi <- transform(indeksi, Indeks3 = (d-c)*100/c)
+indeksi <- indeksi[c(1,6,7,8)] 
+colnames(indeksi) <- c("Vrsta_pogona", 2017:2019)
+
+indeksi <- melt(indeksi, id.vars = "Vrsta_pogona", measure.vars = names(indeksi)[-1],
+                variable.name = "Leto", value.name = "Indeks")
+
+
+# POVPREČNA MESEČNA NETO PLAČA PO REGIJAH
+
+placa <- read_delim("podatki/Povprecna_mesecna_neto_placa_po_regijah.csv", delim = ";", col_names = c("Regija", 2007:2019),
+                    skip=5, n_max=12, locale = locale(encoding = "Windows-1250"))
+placa$Regija <- gsub("Jugovzhodna Slovenija", "Jugovzhodna", placa$Regija)
+
+placa <- placa[c(1,2,3,4,5,6,7,9,10,8,11,12),]
+placa <- melt(placa, id.vars = "Regija", measure.vars = names(placa)[-1],
+              variable.name = "Leto", value.name = "Placa")
+
+placa2015 <- filter(placa, Leto == '2015')
+placa2015 <- placa2015[,-2]
+placa2010 <- filter(placa, Leto == '2010')
+placa2010 <- placa2010[,-1]
+
+
+# ŠTEVILO UMRLIH V CESTNOPROMETNIH NESREČAH NA 10.000 REGISTRIRANIH MOTORNIH VOZIL
+
+umrli <- read_delim("podatki/Stevilo_umrlih_v_cestnoprometnih_nesrecah_na_10.000_registriranih_motornih_vozil.csv", delim=";", col_names = c("nekaj","Regija", 2002:2018),
+                    skip=4, n_max=12, locale = locale(encoding = "Windows-1250"))
+umrli <- umrli[,-1]
+umrli$Regija <- gsub("Jugovzhodna Slovenija", "Jugovzhodna", umrli$Regija)
+umrli$Regija <- gsub("Spodnjeposavska", "Posavska", umrli$Regija)
+umrli$Regija <- gsub("Notranjsko-kraška", "Primorsko-notranjska", umrli$Regija)
+
+
+umrli <- melt(umrli, id.vars = "Regija", measure.vars = names(umrli)[-1],
+              variable.name = "Leto", value.name = "Umrli")
+umrli$Leto <- as.numeric(levels(umrli$Leto))[umrli$Leto]
+
+
+
+# ŠTEVILO OSEBNIH AVTOMOBILOV NA 1000 PREBIVALCEV PO REGIJAH
+
+avtomobili <-read_delim("podatki/Stevilo_osebnih_avtomobilov_na_1000_prebivalcev.csv", delim=";", col_names = c("nekaj","Regija", 2003:2019),
+                        skip=4, n_max=12, locale = locale(encoding = "Windows-1250")) 
+avtomobili <- avtomobili[,-1]
+avtomobili$Regija <- gsub("Jugovzhodna Slovenija", "Jugovzhodna", avtomobili$Regija)
+avtomobili$Regija <- gsub("Notranjsko-kraška", "Primorsko-notranjska", avtomobili$Regija)
+avtomobili$Regija <- gsub("Spodnjeposavska", "Posavska", avtomobili$Regija)
+
+avtomobili <- melt(avtomobili, id.vars = "Regija", measure.vars = names(avtomobili)[-1],
+                   variable.name = "Leto", value.name = "Avtomobili")
+
+avtomobili$Leto <- as.numeric(levels(avtomobili$Leto))[avtomobili$Leto]
+
+# POVPREČNA STAROST OSEBNEGA AVTOMOBILA PO REGIJAH
+
+starost <- read_delim("podatki/Povprecna_starost_osebnih_avtomobilov.csv", delim=";", col_names = c("nekaj","Regija", 2003:2019),
+                      skip=4, n_max=12, locale = locale(encoding = "Windows-1250"))
+starost <- starost[,-1]
+starost$Regija <- gsub("Jugovzhodna Slovenija", "Jugovzhodna", starost$Regija)
+starost$Regija <- gsub("Notranjsko-kraška", "Primorsko-notranjska", starost$Regija)
+starost$Regija <- gsub("Spodnjeposavska", "Posavska", starost$Regija)
+
+starost <- melt(starost, id.vars = "Regija", measure.vars = names(starost)[-1],
+                variable.name = "Leto", value.name = "Starost_avtomobila")
+
+starost2019 <- filter(starost, Leto == '2019')
